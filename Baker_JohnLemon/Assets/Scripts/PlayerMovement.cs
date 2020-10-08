0e9845d282877206df519d0e6c376650d3e36478
garytabker <GeeBake@outlook.com>
2020-10-08 18:20:32 -0400

    finished playermovement script

--- Baker_JohnLemon/Assets/Scripts/PlayerMovement.cs
+++ Baker_JohnLemon/Assets/Scripts/PlayerMovement.cs
@@ -9,7 +9,7 @@ public class PlayerMovement : MonoBehaviour
     Animator m_Animator;   //the animator of John Lemon
     Rigidbody m_Rigidbody; //the rigidbody of John lemon
     Vector3 m_Movement;  //the movement saved as a vector3 from the input
-    Quaternion m_Rotation = Quaternion.identity;
+    Quaternion m_Rotation = Quaternion.identity;  //setting rotation to the default
 
     void Start()
     {
@@ -32,12 +32,12 @@ public class PlayerMovement : MonoBehaviour
         m_Animator.SetBool("IsWalking", isWalking);  //then this sets the isWalking parameter for the animation to true if the player is walking
 
         Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);  //setting the direction we want John lemon to face and slowing his turning down
-        m_Rotation = Quaternion.LookRotation(desiredForward);  //actually rotating john lemon
+        m_Rotation = Quaternion.LookRotation(desiredForward);  //actually setting the rotation to what we want it to be
     }
 
-    void OnAnimatorMove()
+    void OnAnimatorMove()  //this function is needed to move and rotate an animated character
     {
-        m_Rigidbody.MovePosition(m_Rigidbody.position + m_Movement * m_Animator.deltaPosition.magnitude);
-        m_Rigidbody.MoveRotation(m_Rotation);
+        m_Rigidbody.MovePosition(m_Rigidbody.position + m_Movement * m_Animator.deltaPosition.magnitude);  //moving John lemon to the new position
+        m_Rigidbody.MoveRotation(m_Rotation);  //rotating john lemon
     }
 }
