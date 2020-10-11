5fd9f740f8b3c7509a95d282e7c040f49c272ff9
garytabker <GeeBake@outlook.com>
2020-10-11 12:30:49 -0400

    changing waypoints so the ghost keeps moving in update of Waypoint Control

--- Baker_JohnLemon/Assets/Scripts/WaypointPatrol.cs
+++ Baker_JohnLemon/Assets/Scripts/WaypointPatrol.cs
@@ -7,6 +7,10 @@ public class WaypointPatrol : MonoBehaviour
 {
     public NavMeshAgent navMeshAgent;  //the nav mesh agent of the ghost
     public Transform[] waypoints;  //the set of waypoints the ghost will go through
+
+    int m_CurrentWaypointIndex;  //the index of the array that we are at
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +20,10 @@ public class WaypointPatrol : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (navMeshAgent.remainingDistance < navMeshAgent.stoppingDistance)  //checking to see if the navmeshagent has arrived at its destination
+        {
+            m_CurrentWaypointIndex = (m_CurrentWaypointIndex + 1) % waypoints.Length;  //this iterates the index by one, but also makes it circle back by using modulus
+            navMeshAgent.SetDestination(waypoints[m_CurrentWaypointIndex].position); //sets the new destination
+        }
     }
 }
