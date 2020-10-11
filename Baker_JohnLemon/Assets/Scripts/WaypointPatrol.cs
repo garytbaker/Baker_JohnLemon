682fe377c85069d8b53517e6dc0907aa2847e332
garytabker <GeeBake@outlook.com>
2020-10-11 12:27:27 -0400

    starting in on waypoint patrol script - variables declarations

--- Baker_JohnLemon/Assets/Scripts/WaypointPatrol.cs
+++ Baker_JohnLemon/Assets/Scripts/WaypointPatrol.cs
@@ -1,13 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class WaypointPatrol : MonoBehaviour
 {
+    public NavMeshAgent navMeshAgent;  //the nav mesh agent of the ghost
+    public Transform[] waypoints;  //the set of waypoints the ghost will go through
     // Start is called before the first frame update
     void Start()
     {
-        
+        navMeshAgent.SetDestination(waypoints[0].position);  //setting the destination to the first thing in the array of waypoints
     }
 
     // Update is called once per frame
