9f2c5b0e3d4a821ea91e93368c74698cefcaf5e2
garytabker <GeeBake@outlook.com>
2020-10-11 12:53:47 -0400

    working in endlevel script - adding references to the sound

--- Baker_JohnLemon/Assets/Scripts/GameEnding.cs
+++ Baker_JohnLemon/Assets/Scripts/GameEnding.cs
@@ -10,9 +10,12 @@ public class GameEnding : MonoBehaviour
     public GameObject player;   //the player gameobjects reference
     public CanvasGroup exitBackgroundImageCanvasGroup;  //the canvas group for the fade to black and image
     public CanvasGroup caughtBackgroundImageCanvasGroup; // canvas group dor when the player is caught
+    public AudioSource exitAudio;  //different sound effects
+    public AudioSource caughtAudio;
 
     bool m_IsPlayerAtExit;  //the boolean that determines if the player is at the exit
     bool m_IsPlayerCaught;  //bool determining if the player is caught
+    bool m_HasAudioPlayed;  //bool determining if the audio has played
 
     float m_Timer;          //the timer to see how long we are at the end of the level
 
@@ -33,19 +36,21 @@ public class GameEnding : MonoBehaviour
     {
         if (m_IsPlayerAtExit)  //if the player is at teh end of the level
         {
-            EndLevel(exitBackgroundImageCanvasGroup, false);  //then we will end the level
+            EndLevel(exitBackgroundImageCanvasGroup, false,exitAudio);  //then we will end the level
         }
         else if (m_IsPlayerCaught)  //if the player is caught
         {
-            EndLevel(caughtBackgroundImageCanvasGroup, true);  //edn the level
+            EndLevel(caughtBackgroundImageCanvasGroup, true, caughtAudio);  //edn the level
         }
     }
 
     /// <summary>
     /// this function actually ends the game
     /// <paramref name="imageCanvasGroup"/>the canvas group is what you want to fade in
+    /// <paramref name="doRestart"/>if teh game should restart or not
+    /// <paramref name="audioSource"/>the audio source to be played
     /// </summary>
-    void EndLevel(CanvasGroup imageCanvasGroup, bool doRestart)
+    void EndLevel(CanvasGroup imageCanvasGroup, bool doRestart, AudioSource audioSource)
     {
         m_Timer += Time.deltaTime;  //iterates the time making a makeshift timer
 
