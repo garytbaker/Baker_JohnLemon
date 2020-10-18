8b27a43dcc32e23dcf235485346397e1a7396514
garytabker <GeeBake@outlook.com>
2020-10-18 18:37:32 -0400

    working on detection of cubes by platforms

--- Baker_JohnLemon/Assets/Scripts/CubeDestAdder.cs
+++ Baker_JohnLemon/Assets/Scripts/CubeDestAdder.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class CubeDestAdder : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.tag == "GamePiece")
+        {
+            
+        }
     }
 }
