a87051cad2baa9c5ca5e5a9428a1d8f9c1a79e2b
garytabker <GeeBake@outlook.com>
2020-10-18 18:49:18 -0400

    created script containg global ideas

--- Baker_JohnLemon/Assets/Scripts/CubeDestAdder.cs
+++ Baker_JohnLemon/Assets/Scripts/CubeDestAdder.cs
@@ -18,7 +18,10 @@ public class CubeDestAdder : MonoBehaviour
         {
             Vector2 GamePieceXZ = new Vector2(other.transform.position.x, other.transform.position.y);
             float distanceBetween = Vector2.Distance(PlatformXZ, GamePieceXZ);
-            
+            if (distanceBetween<2.0f)
+            {
+
+            }
        
         }
     }
