e0f573557451bc2ffa76908b004b430f936f0139
garytabker <GeeBake@outlook.com>
2020-10-20 12:34:58 -0400

    helping clean up the code

--- Baker_JohnLemon/Assets/Scripts/GlobalVariables.cs
+++ Baker_JohnLemon/Assets/Scripts/GlobalVariables.cs
@@ -6,32 +6,42 @@ using UnityEngine.SceneManagement;
 public class GlobalVariables : MonoBehaviour
 {
 
-    public int numberOfPlatforms;
-    public int goalNumberOfPlatforms;
-    public GameObject ghost;
-    private bool spawned;
-    public float spawnTimer = 60.0f;
-    public Observer pointOfView;
+    public int numberOfPlatforms;  //the number of platforms that currently have a box on them
+    public int goalNumberOfPlatforms;  //the number needed to go to the next level
+    public GameObject ghost;  //the ghost that will spawn
+    private bool spawned;  //if the ghost has spawned already
+    public float spawnTimer = 60.0f;  //how long until the ghost will spawn. base is one minute
+    public Observer pointOfView;  //this is the point of view of the ghost that is spawned
+
+    /// <summary>
+    /// This is a function that adds one to the number of platforms with boxes on them
+    /// </summary>
     public void addPlatform()
     {
         numberOfPlatforms += 1;
-        if (numberOfPlatforms == goalNumberOfPlatforms)
+        if (numberOfPlatforms == goalNumberOfPlatforms)  //if the number is the goal
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);  //go to the next level
         }
     }
+
+    /// <summary>
+    /// This is to take off a platform when a boxe moves off of the platform
+    /// </summary>
     public void deletePlatform()
     {
         numberOfPlatforms -= 1;
     }
 
+
+    //using update as the timer function
     public void Update()
     {
-        spawnTimer -= Time.deltaTime;
-        if (spawnTimer <= 0.0f && spawned ==false)
+        spawnTimer -= Time.deltaTime;//iterates the time
+        if (spawnTimer <= 0.0f && spawned ==false) //if the timer has been reached
         {
-            spawned = true;
-            spawnGhost();
+            spawned = true;  //set spawned to true to only spawn one ghost
+            spawnGhost();  //then spawn the ghost
         }
     }
 
