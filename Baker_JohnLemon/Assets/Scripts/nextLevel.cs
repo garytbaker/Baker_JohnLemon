0725774972a20afd506ab10abeea42a93b04926e
garytabker <GeeBake@outlook.com>
2020-10-25 17:21:24 -0400

    adding way to go from instruction page to next level

---
+++ Baker_JohnLemon/Assets/Scripts/nextLevel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class nextLevel : MonoBehaviour
+{
+    int levelNumber; //the build index of the current level
+    // Start is called before the first frame update
+    void Start()
+    {
+        levelNumber = SceneManager.GetActiveScene().buildIndex;
+        if (levelNumber == 0)
+        {
+            levelNumber = 1;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene(levelNumber);
+        }
+    }
+}
